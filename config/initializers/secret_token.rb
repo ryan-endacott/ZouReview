5c3fe79a648600cd72275c6a88df4ad17d3b8bac
Ryan Endacott <rzeg24@gmail.com>
2013-02-06 17:22:34 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mizzouclass::Application.config.secret_token = 'dceba89e94d58923984d01ea727584f25aee64f85c39ff3a069e25b16b086a6057c061a15915eafb4dcf4abeb4e5bd336453aef88907d7bddd4259b2e489dd94'
