531cea617bc6e78cb7b4b9c4b427acac34777a5a
Ryan Endacott <rzeg24@gmail.com>
2013-02-07 16:15:19 -0600

    Added my first real test! Tests (nonexistent!) method GradeCrawler.get_post_string

--- spec/lib/grade_crawler_spec.rb
+++ spec/lib/grade_crawler_spec.rb
@@ -3,13 +3,18 @@ require 'grade_crawler'
 
 describe GradeCrawler do
 
-  it "should have accessible name attribute" do
-    name1 = "fred"
-    name2 = "sue"
-    crawler = GradeCrawler.new(name1)
-    crawler.name.should == name1
-    crawler.name = name2
-    crawler.name.should == name2
+  describe 'get_post_string' do
+    it 'should return a valid post string' do
+
+      GradeCrawler.stub(:get_current_term).and_return 'FS2012'
+
+      def GradeCrawler.pub_get_post_string
+        get_post_string
+      end
+
+      GradeCrawler.pub_get_post_string.should == 'vterm=&vinstructor=ABAD%2CNEETU+S&vclass=&vdept=&vtitle=&vbool=&vcomnumb='
+
+    end
   end
 
 end
