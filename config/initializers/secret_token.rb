e49d9d16f3e42638d94fb26dd7770d981bfcf5b1
Ryan Endacott <rzeg24@gmail.com>
2013-02-10 22:48:05 -0600

    Added database configuration to config and changed config/database.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,4 @@ end
 # no regular words or you'll be exposed to dictionary attacks.
 Mizzouclass::Application.config.secret_token = 
   ENV['SECRET_TOKEN'] || 'dceba89e94d58923984d01ea727584f25aee64f85c39ff3a069e25b16b086a6057c061a15915eafb4dcf4abeb4e5bd336453aef88907d7bddd4259b2e489dd94'
+
