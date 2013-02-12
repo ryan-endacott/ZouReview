ff8360e44e67edab77d486a556a10db1c5d6a44a
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 18:26:34 -0600

    Associate courses and instructors!

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ActiveRecord::Base
   belongs_to :department
 
   has_many :sections
+  has_many :instructors, :through => :sections
 
   attr_accessible :number, :title
 
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,7 @@
 class Instructor < ActiveRecord::Base
 
   has_many :sections
+  has_many :courses, :through => :sections
 
   attr_accessible :name
 
