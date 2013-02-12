a29590b18ef87bf670434b5a37d3c6fc58dcd737
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 21:19:04 -0600

    Test for instructor validation presence of name

--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -4,4 +4,6 @@ describe Instructor do
   it {should have_many :sections}
   it {should have_many(:courses).through :sections}
   it {should have_many(:departments).through :courses}
+
+  it {should validate_presence_of :name}
 end
