83c4516e3a311bee6fb85ac20cae163152e73889
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 20:57:50 -0600

    Passing section validation tests!

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,4 +7,11 @@ class Section < ActiveRecord::Base
 
   attr_accessible :avg_gpa, :num_a, :num_b, :num_c, :num_d, :num_f, :number, :term
 
+  validates_presence_of :avg_gpa, :num_a, :num_b, :num_c, :num_d,
+    :num_f, :number, :term, :course, :instructor, :department
+
+  validates :num_a, :num_b, :num_c, :num_d, :num_f, :numericality => {:only_integer => true}
+
+  validates :avg_gpa, :numericality => true
+
 end
