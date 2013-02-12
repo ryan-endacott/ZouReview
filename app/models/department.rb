05b9eab0335fce9044d5d4356927ee26b528e1ac
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 18:31:24 -0600

    Last association done!  Departments to instructors!!!

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,6 +2,7 @@ class Department < ActiveRecord::Base
 
   has_many :courses
   has_many :sections, :through => :courses
+  has_many :instructors, :through => :sections
 
   attr_accessible :name
 
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,6 +2,7 @@ class Instructor < ActiveRecord::Base
 
   has_many :sections
   has_many :courses, :through => :sections
+  has_many :departments, :through => :courses
 
   attr_accessible :name
 
