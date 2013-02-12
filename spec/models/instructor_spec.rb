69dae39cfb80a2cf5e6a469684143150815e4795
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 21:35:36 -0600

    Instructor should give a name of "staff" if name is blank

--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -6,4 +6,11 @@ describe Instructor do
   it {should have_many(:departments).through :courses}
 
   it {should validate_presence_of :name}
+
+  it 'should give a name of "staff" if name is blank' do
+    i = Instructor.new
+    i.name = ''
+    i.save
+    i.name.should == 'Staff'
+  end
 end
