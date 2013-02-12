67953f8e90d8cf309a466ff0cc2852e77d9f91ab
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 18:15:58 -0600

    Added section to department association

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,7 @@
 class Department < ActiveRecord::Base
 
   has_many :courses
+  has_many :sections, :through => :courses
 
   attr_accessible :name
 
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,8 @@
 class Section < ActiveRecord::Base
 
   belongs_to :course
+  
+  has_one :department, :through => :course
 
   attr_accessible :avg_gpa, :num_a, :num_b, :num_c, :num_d, :num_f, :number, :term
 
