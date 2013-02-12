36038c7f615e145aae1633270ecbfafd408502ce
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 20:35:09 -0600

    Numericality validation tests for section model

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -4,4 +4,18 @@ describe Section do
   it {should belong_to :instructor}
   it {should belong_to :course}
   it {should have_one :department}
+
+  it {should validate_presence_of :num_a}
+  it {should validate_presence_of :num_b}
+  it {should validate_presence_of :num_c}
+  it {should validate_presence_of :num_d}
+  it {should validate_presence_of :num_f}
+  it {should validate_presence_of :avg_gpa}
+
+  it {should validate_numericality_of(:num_a).only_integer}
+  it {should validate_numericality_of(:num_b).only_integer}
+  it {should validate_numericality_of(:num_c).only_integer}
+  it {should validate_numericality_of(:num_d).only_integer}
+  it {should validate_numericality_of(:num_f).only_integer}
+  it {should validate_numericality_of :avg_gpa}
 end
