03b5005d93bd09f387ee9a744b96a5662ccc56e8
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 21:54:48 -0600

    Dept should validate presence of abbreviation

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -4,4 +4,6 @@ describe Department do
   it {should have_many :courses}
   it {should have_many(:sections).through :courses}
   it {should have_many(:instructors).through :sections}
+
+  it {should validate_presence_of :abbreviation}
 end
