1c880dc559fb36818c8a889be6d5bd38855d834b
Ryan Endacott <rzeg24@gmail.com>
2013-02-11 21:47:28 -0600

    Course should validate presence of title and number

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,4 +4,7 @@ describe Course do
   it {should have_many :sections}
   it {should have_many(:instructors).through :sections}
   it {should belong_to :department}
+
+  it {should validate_presence_of :title}
+  it {should validate_presence_of :number}
 end
