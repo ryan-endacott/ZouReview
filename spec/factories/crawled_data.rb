15760489f00c0af05317090ec7e56b7e0d7dd012
Ryan Endacott <rzeg24@gmail.com>
2013-02-13 20:36:15 -0600

    Factory to represent sample crawled data

---
+++ spec/factories/crawled_data.rb
@@ -0,0 +1,19 @@
+# Factory to represent crawled and parsed data from grade distribution site
+
+FactoryGirl.define do
+  factory :crawled_data, class:Hash do
+    course_dept 'HP'
+    course_title 'PHYSICAL AGENTS'
+    course_number '214'
+    section_number '1'
+    term 'WS2001'
+    course_au 'HP'
+    instructor 'ABBOTT'
+    count_a 12
+    count_b 22
+    count_c 0
+    count_d 2
+    count_f 0
+    avg_gpa 3.353
+  end
+end
