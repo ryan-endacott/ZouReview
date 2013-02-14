933293bb0d66ee58f9e78f9add6d9b36f4a8e853
Ryan Endacott <rzeg24@gmail.com>
2013-02-13 19:41:05 -0600

    Factory girl config setting

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,11 @@ module Mizzouclass
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    # Factory girl 
+    config.generators do |g|
+        g.fixture_replacement :factory_girl
+    end
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
