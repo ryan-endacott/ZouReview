242b96b44edfbcb7fc6b1ce48283f0430604234e
Ryan Endacott <rzeg24@gmail.com>
2013-02-19 17:10:35 -0600

    Tests for Course#calculate_avg_gpa!

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -7,4 +7,26 @@ describe Course do
 
   it {should validate_presence_of :title}
   it {should validate_presence_of :number}
+
+  describe 'calculate_avg_gpa!' do
+
+    let!(:course) { FactoryGirl.create(:course) }
+    let(:section1) { FactoryGirl.create(:section, :course_id => course.id, :avg_gpa => 3.5) }
+    let(:section2) { FactoryGirl.create(:section, :course_id => course.id, :avg_gpa => 3.2) }
+    let(:avg_gpa) { 3.35 }
+
+    before(:each) do
+      course.calculate_avg_gpa!
+    end
+
+    it 'should correctly calculate gpa' do
+      course.avg_gpa.should == avg_gpa
+    end
+
+    it 'should resave to database after calculating' do
+      Course.find(course).avg_gpa.should == avg_gpa
+    end
+
+  end
+
 end
