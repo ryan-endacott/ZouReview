1581103bf4f26e50e0595834b870444ec7007221
Ryan Endacott <rzeg24@gmail.com>
2013-02-23 01:27:11 -0600

    Super basic beginnings of an admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Mizzouclass::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :admin
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
