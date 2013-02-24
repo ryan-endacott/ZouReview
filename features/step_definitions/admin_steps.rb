c7e72fc49af7b5b0e075912a42080fd8e4405d8b
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 17:03:00 -0600

    Admin_steps

---
+++ features/step_definitions/admin_steps.rb
@@ -0,0 +1,24 @@
+require 'grade_crawler'
+
+Given /I fill in a[n]? (in)?correct password/ do |incorrect|
+  fill_in(:password, :with => ENV['SECRET_PASSWORD']) unless incorrect
+end
+
+And /I submit with term "(.*)"/ do |term|
+  print Rails.root
+  print 'rails root above\n'
+  #https://musis1.missouri.edu/gradedist/mu_grade_dist_intro.cfm#CGI.script.name
+  stub_request(:any, GradeCrawler::SITE_URI)
+    .to_return(:body => File.new(Rails.root.join('spec', 'support', 'sample_grade_page_2.html')), :status => 200)
+  fill_in(:term, :with => term)
+  click_button('Submit')
+end
+
+Then /there should (not )?be sections with "(.*)"/ do |none, term|
+  visit(sections_path)
+  if none
+    page.should have_no_content(term)
+  else
+    page.should have_content(term)
+  end
+end
