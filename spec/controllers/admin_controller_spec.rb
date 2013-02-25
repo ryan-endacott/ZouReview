677baa7a21012dafb2615945c18d5b0d43c6fc26
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 18:25:56 -0600

    Start of a spec for the admin controller

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,4 +2,34 @@ require 'spec_helper'
 
 describe AdminController do
 
+  describe '#crawl_grades' do
+
+    context 'correct password' do
+
+      it 'should correctly start a job to crawl the grades' do
+        pending 'GradeCrawlerJob needs to be made'
+      end
+
+      it 'should render a success message' do
+        pending 'GradeCrawlerJob needs to be made'
+      end
+
+      after(:each) do
+        post :crawl_grades, :term => 'FS2010', :password => ENV['SECRET_PASSWORD']
+      end
+    end
+
+    context 'incorrect password' do
+
+      it 'should redirect to the admin page with an error' do
+
+      end
+
+      after(:each) do
+        post :crawl_grades, :term => 'FS2010', :password => 'lol'
+      end
+    end
+
+  end
+
 end
