0849d1664e1184b4530bbe6cafcff763baba7d44
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 20:00:04 -0600

    Fixed have_css selectors for success and error messages

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -9,11 +9,12 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
 end
 
 Then /I should see a success message/ do
-  page.should have_css('alert-success')
+  print page.body
+  page.should have_css('.alert-success')
 end
 
 Then /I should see an error message/ do
-  page.should have_css('alert-error')
+  page.should have_css('.alert-error')
 end
 
 
