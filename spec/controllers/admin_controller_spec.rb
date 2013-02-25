1cb9271728889b3156a1cae2c8c091a351748892
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 19:34:33 -0600

    admin controller spec test pretty much complete

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,25 +1,30 @@
 require 'spec_helper'
+require 'grade_crawler_job'
 
 describe AdminController do
 
   describe '#crawl_grades' do
 
+    let(:grade_crawler_job) { GradeCrawlerJob.new 'FS2010' }
+
     before(:each) do
       Delayed::Job.stub(:enqueue)
+      GradeCrawlerJob.stub(:new).and_return grade_crawler_job
     end
 
     it 'should render the index view' do
       expect(response).to render_template('index')
+      post :crawl_grades
     end
 
     context 'correct password' do
 
       it 'should correctly start a job to crawl the grades' do
-        pending 'GradeCrawlerJob needs to be made'
+        Delayed::Job.should_receive(:enqueue).with(grade_crawler_job)
       end
 
       it 'should render a success message' do
-        pending 'GradeCrawlerJob needs to be made'
+        flash[:success].should_not be_nil
       end
 
       after(:each) do
@@ -30,11 +35,11 @@ describe AdminController do
     context 'incorrect password' do
 
       it 'should redirect to the admin page with an error' do
-
+        flash[:error].should_not be_nil
       end
 
       it 'should not start a job to crawl grades' do
-
+        Delayed::Job.should_not_receive(:enqueue)
       end
 
       after(:each) do
