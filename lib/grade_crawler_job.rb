333ac9ea78751179f2ae09ced2e72acef4247088
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 19:13:53 -0600

    GradeCrawlerJob complete!  Passing tests

---
+++ lib/grade_crawler_job.rb
@@ -0,0 +1,14 @@
+# Delayed::Job job to process grade crawls in the background
+# Start a background grade crawl by doing:
+# Delayed::Job.enqueue GradeCrawlerJob.new(term)
+
+require 'grade_crawler'
+require 'section_creator'
+
+class GradeCrawlerJob < Struct.new(:term)
+  def perform
+    data = GradeCrawler.get_grade_data(term)
+    section_creator = SectionCreator.new(data)
+    section_creator.create_sections!
+  end
+end
