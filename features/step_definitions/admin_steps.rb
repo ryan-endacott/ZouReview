f70ab2df5c13d785dd239d05da08cc5ebf14a997
Ryan Endacott <rzeg24@gmail.com>
2013-02-24 20:52:17 -0600

    Fixed a couple things in admin_steps and.....  Passing tests!!  A green cucumber!

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -5,18 +5,21 @@ Given /I fill in a[n]? (in)?correct password/ do |incorrect|
 end
 
 And /I submit with term "(.*)"/ do |term|
-  print Rails.root
-  print 'rails root above\n'
-  #https://musis1.missouri.edu/gradedist/mu_grade_dist_intro.cfm#CGI.script.name
-  stub_request(:any, GradeCrawler::SITE_URI)
+
+  stub_request(:any, 'https://musis1.missouri.edu/gradedist/mu_grade_dist_intro.cfm')
     .to_return(:body => File.new(Rails.root.join('spec', 'support', 'sample_grade_page_2.html')), :status => 200)
   fill_in(:term, :with => term)
   click_button('Submit')
+
 end
 
 Then /there should (not )?be sections with "(.*)"/ do |none, term|
-  # Check database for new sections with that term
-  Sections.where(:term => term).should_not be_empty
+  # Check database for new sections with that term if not none
+  if none
+    Section.where(:term => term).should be_empty
+  else
+    Section.where(:term => term).should_not be_empty
+  end
 
   # This code below should probably be used instead of above once section scaffolding and stuff is 
   # completed.  Otherwise, this feature won't pass.
