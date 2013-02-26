7537b325e6fa018bf2f63a76f3d001766cc4eab6
Ryan Endacott <rzeg24@gmail.com>
2013-02-25 21:12:03 -0600

    Test for admin controller list of comma separated terms

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -6,10 +6,12 @@ describe AdminController do
   describe '#crawl_grades' do
 
     let(:grade_crawler_job) { GradeCrawlerJob.new 'FS2010' }
+    let(:grade_crawler_job2) { GradeCrawlerJob.new 'FS2011' }
 
     before(:each) do
       Delayed::Job.stub(:enqueue)
-      GradeCrawlerJob.stub(:new).and_return grade_crawler_job
+      GradeCrawlerJob.stub(:new).with('FS2010').and_return grade_crawler_job
+      GradeCrawlerJob.stub(:new).with('FS2011').and_return grade_crawler_job2
     end
 
     it 'should render the index view' do
@@ -22,6 +24,12 @@ describe AdminController do
       it 'should correctly start a job to crawl the grades' do
         Delayed::Job.should_receive(:enqueue).with(grade_crawler_job)
         post :crawl_grades, :term => 'FS2010', :password => ENV['SECRET_PASSWORD']
+      end
+
+      it 'should also be able to take a comma separated list of terms' do
+        Delayed::Job.should_receive(:enqueue).with(grade_crawler_job)
+        Delayed::Job.should_receive(:enqueue).with(grade_crawler_job2)
+        post :crawl_grades, :term => 'FS2010, FS2011', :password => ENV['SECRET_PASSWORD']
       end
 
       it 'should render a success message' do
