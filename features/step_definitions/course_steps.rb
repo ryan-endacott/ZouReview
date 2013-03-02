ff6f3a57a563060264b27fc617794410c67efb16
Ryan Endacott <rzeg24@gmail.com>
2013-03-01 19:27:12 -0600

    Start of course steps

---
+++ features/step_definitions/course_steps.rb
@@ -0,0 +1,13 @@
+
+
+Given /the following courses exist/ do |courses_table|
+  courses_table.hashes.each do |course|
+    c = Course.new
+    c.title = course[:title]
+    c.number = course[:number]
+    # Unfortunate hack..... Because avg_gpa can only be set privately
+    # (rightly so, but sucks for testing...)
+    c.send(:avg_gpa=, course[:avg_gpa])
+    c.save!
+  end
+end
