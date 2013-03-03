bb9fbe940b91addae54801a91cd2efb30fb440a1
Ryan Endacott <rzeg24@gmail.com>
2013-03-03 01:56:14 -0600

    Got section to work with custom validation for course existing!  Now must fix a couple edge cases that somehow got affected by this?!?!

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,10 +6,30 @@ class Section < ActiveRecord::Base
   has_one :department, :through => :course
 
   validates_presence_of :avg_gpa, :num_a, :num_b, :num_c, :num_d,
-    :num_f, :number, :term, :course, :instructor
+    :num_f, :number, :term
+
+  # This validation must be used instead of validates_presence_of (course or instructor)
+  # Because when creating with Section.where(:course_id => 1).first_or_create, it would
+  # Fails and be invalid because no real 'course' supplied
+  validate :course_or_course_id
+  validate :instructor_or_instructor_id
 
   validates :num_a, :num_b, :num_c, :num_d, :num_f, :numericality => {:only_integer => true}
 
   validates :avg_gpa, :numericality => true
+
+  private
+
+  def course_or_course_id
+    if !(course_id || course)
+      errors.add(:course, 'Section must have a course or course_id.')
+    end
+  end
+
+  def instructor_or_instructor_id
+    if !(instructor_id || instructor)
+      errors.add(:instructor, 'Section must have an instructor or instructor_id.')
+    end
+  end
 
 end
