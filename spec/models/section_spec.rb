a135d87ac8015f764bc595fa5a9c8c27b863cf03
Ryan Endacott <rzeg24@gmail.com>
2013-03-03 00:45:18 -0600

    Tests for validating a section with course OR course_id, same with instructor.  This is due to a Section.where(:course_id => 1).first_or_create not working because it is invalid due to no course.  So this should hopefully fix that!

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -23,4 +23,47 @@ describe Section do
   it {should validate_numericality_of(:num_f).only_integer}
   it {should validate_numericality_of :avg_gpa}
 
+  # These ended up necessary because of where clause with a first_or_create
+  # not validating because there was no 'course', only a course_id
+
+  context 'validate presence of course or course_id' do
+    let(:section) { FactoryGirl.build(:section, :course => nil) }
+    let(:course) { FactoryGirl.build(:course) }
+
+    it 'the original let clause should be invalid' do
+      section.should_not be_valid
+    end
+
+    it 'should validate with just a course_id' do
+      section.course_id = 1
+      section.should be_valid
+    end
+
+    it 'should validate with just a course' do
+      section.course = course
+      section.should be_valid
+    end
+
+  end
+
+  context 'validate presence of instructor or instructor_id' do
+    let(:section) { FactoryGirl.build(:section, :instructor => nil) }
+    let(:instructor) { FactoryGirl.build(:instructor) }
+
+    it 'the original let clause should be invalid' do
+      section.should_not be_valid
+    end
+
+    it 'should validate with just an instructor_id' do
+      section.instructor_id = 1
+      section.should be_valid
+    end
+
+    it 'should validate with just an instructor' do
+      section.instructor = instructor
+      section.should be_valid
+    end
+
+  end
+
 end
